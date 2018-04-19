3ed91ba512a006ce4fa18d628e16406c502b3d1b
Steve Rodriguez <silent12ill@gmail.com>
2018-04-18 23:35:37 -0500

    setup fuction to pass file into into a queue, set interval for queue files to be sent to ftp

--- app/app.js
+++ app/app.js
@@ -1,12 +1,61 @@
 var chokidar = require('chokidar');
+var PromiseFtp = require('promise-ftp');
 
-// set chokidar to watch watchedDir
-chokidar.watch('app/watchedDir/*', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+var ftp = new PromiseFtp();
 
-  console.log(event, path);
+//queue for items to send to ftp remote dir
+var queue = [];
 
+function sendFile() {
+  console.log("Sendfile Running");
+  //check if something is in queue
+  if(queue[0]){
+    let currentFile = queue.pop();
+
+    //Send file via ftp
+    ftp.put(currentFile.path, currentFile.filename).then(function(){
+      //Output the current file being sent
+      console.log("File" + currentFile.filename + " sent");
+      return ftp.list('/');
+    }).then(function (list) {
+      //Check if File exists in destination maybe compare size
+      //If file does not exist add back to queue
+      //Else delete original file in source location
+      console.log(queue);
+    });
+  }
+};
+
+//Setting interval to check if something needs to be sent
+setInterval(sendFile, 3500);
+
+//ftp server info and credentials
+var serverInfo = {
+  host: "ftp.dlptest.com",
+  user: "dlpuser@dlptest.com",
+  password: "eiTqR7EMZD5zy7M"
+};
+
+// start ftp with promise into watcher
+ftp.connect(serverInfo).then(function (serverMessage) {
+
+  //Output server message if FTP has one
+  console.log(serverMessage);
+
+  // set chokidar to watch watchedDir
+  chokidar.watch('app/watchedDir/*', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+
+    //output event and path
+    console.log(event, path);
+    if (event == "add") {
+      let filename = path.split('/')[2];
+      let fileInfo = {
+        path: path,
+        filename: filename
+      };
+      queue.push(fileInfo);
+    }
+  });
 });
-
-
 
 console.log("Ayyyye");
